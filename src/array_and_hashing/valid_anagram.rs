1543387c4252de3160f06ae94da0b0e9ecdab375
tomoya ishigaki <steinwand.tomo@gmail.com>
2023-02-23 15:58:23 +0900

    improve logic of probrem2

--- src/array_and_hashing/valid_anagram.rs
+++ src/array_and_hashing/valid_anagram.rs
@@ -1,7 +1,28 @@
+use std::collections::HashMap;
+
 use crate::Solution;
 
 impl Solution {
+    // time: O(n)
+    // space: O(n)
     pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len() {
+            return false;
+        }
+        let mut map_s = HashMap::new();
+        let mut map_t = HashMap::new();
+        for i in 0..s.len() {
+            let counter_s = map_s.entry(s.as_bytes()[i]).or_insert(0);
+            let counter_t = map_t.entry(t.as_bytes()[i]).or_insert(0);
+            *counter_s += 1;
+            *counter_t += 1;
+        }
+        map_s == map_t
+    }
+    #[allow(dead_code)]
+    // time: O(nlogn)
+    // space: O(n)
+    fn is_anagram_sol1(s: String, t: String) -> bool {
         if s.len() != t.len() {
             return false;
         }
@@ -9,12 +30,7 @@ impl Solution {
         sort_s.sort();
         let mut sort_t: Vec<char> = t.chars().collect();
         sort_t.sort();
-        for i in 0..s.len() {
-            if sort_s[i] != sort_t[i] {
-                return false;
-            }
-        }
-        return true;
+        sort_s == sort_t
     }
 }
 
