ad843274f016247077895fd5b549ef07377d5a6e
tomoya ishigaki <steinwand.tomo@gmail.com>
2023-02-25 13:05:13 +0900

    improve logic of problem3 of stack

--- src/stack/eval_rev_polish_notation.rs
+++ src/stack/eval_rev_polish_notation.rs
@@ -2,29 +2,30 @@ use crate::Solution;
 
 impl Solution {
     pub fn eval_rpn(tokens: Vec<String>) -> i32 {
-        let mut stack = vec![];
-
-        tokens.iter().for_each(|s| match s.as_str() {
-            "+" | "-" | "*" | "/" => {
-                let val2 = stack.pop().unwrap();
-                let val1 = stack.pop().unwrap();
-                stack.push(Self::eval_op(s.to_string(), val1, val2).to_string())
+        let mut stack: Vec<i32> = vec![];
+        for token in tokens.iter() {
+            match token.as_str() {
+                "+" | "-" | "*" | "/" => {
+                    Self::eval_op_and_push(&token, &mut stack);
+                }
+                _ => stack.push(token.parse().unwrap()),
             }
-            _ => stack.push(s.to_string()),
-        });
-        stack.pop().unwrap().parse().unwrap()
+        }
+        stack.pop().unwrap()
     }
 
-    fn eval_op(op: String, val1: String, val2: String) -> i32 {
-        let val1: i32 = val1.parse().unwrap();
-        let val2: i32 = val2.parse().unwrap();
-        match op.as_str() {
+    #[inline]
+    fn eval_op_and_push(op: &str, stack: &mut Vec<i32>) {
+        let val2 = stack.pop().unwrap();
+        let val1 = stack.pop().unwrap();
+
+        stack.push(match op {
             "+" => val1 + val2,
             "-" => val1 - val2,
             "*" => val1 * val2,
             "/" => val1 / val2,
             _ => unreachable!(),
-        }
+        });
     }
 }
 
